ad7c467ce21cac1b8ad21616bb3e183765af875f
Chralt98 <chralt98@gmail.com>
2019-03-21 10:25:00 +0100

    Added information. Optimized the class wallet.

--- wallet.py
+++ wallet.py
@@ -2,6 +2,9 @@ from cryptic import MicroService
 import uuid
 import random
 import string
+import sqlite3
+import os
+import sys
 
 
 class Wallet:
@@ -12,10 +15,8 @@ class Wallet:
     amount = 0
     # if the wallet has not been created the key is not activated
     key = 'not activated'
-
     # identifier is the uuid of the wallet
-    def __init__(self, identifier):
-        self.identifier = identifier
+    identifier = 'not activated'
 
     # personal key for sending or receive morph coins
     def get_key(self):
@@ -25,40 +26,57 @@ class Wallet:
     def set_key(self, key):
         self.key = key
 
-    # for checking the amount of morph coins
-    def get_balance(self):
-        return self.amount
+    def set_identifier(self, identifier):
+        self.identifier = identifier
 
-    # creates the wallet with creating a personal key
-    def create_wallet(self):
+    def get_identifier(self):
+        return self.identifier
+
+    # for checking the amount of morph coins
+    def get_balance(self, user_name, key):
+        # TODO: Get the amount out of the database
+        # no valid key -> no status of balance
+        if self.get_key() == 'not activated':
+            return {"error": "You have to create a wallet before sending morph coins!"}
+        return {"balance": self.amount}
+
+    # creates the wallet with creating a personal key and a uuid
+    def create_wallet(self, user_name):
+        # creates an unified unique identifier to identify each wallet
+        self.set_identifier(str(uuid.uuid4()))
         # generate a personal key and set it for the wallet
         # key contains ascii letters and digits and is 10 chars long
         self.set_key(''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)]))
-        return 'Your wallet has been created. This is your personal key: ' + self.get_key()
+        # TODO: put the information in the sqlite3 database
+        return {"status": str(user_name) + " , your wallet has been created. ", "uuid": str(self.get_identifier()),
+                "key": str(self.get_key())}
 
-    def send_coins(self, key, destination, send_amount):
+    def send_coins(self, user_name, key, destination, send_amount):
+        # TODO: Check the user name and key in the sqlite3 database
         # if no random key was generated and the key is still not activated the user will not send coins
         if self.get_key() == 'not activated':
-            return 'You have to create a wallet before sending morph coins!'
+            return {"error": "You have to create a wallet before sending morph coins!"}
         # if the pasted key is not the valid key no transfer will be confirmed
         if str(key) != str(self.get_key()):
-            return 'Transfer failed! You key is not valid.'
+            return {"error": "Transfer failed! You key is not valid."}
         # if the wanted amount to send is higher than the current balance it will fail to transfer
         if send_amount > self.amount:
-            return 'Transfer failed! You have not enough morph coins.'
+            return {"error": 'Transfer failed! You have not enough morph coins.'}
+        # TODO: Update the amount in the database
         # the balance of the wallet is current amount minus send amount of morph coins
         self.amount -= send_amount
         # successful status mail with transfer information
-        return 'Transfer of ' + str(send_amount) + ' from ' + str(self.identifier) +\
-               ' to ' + str(destination) + ' successful!'
+        return {"status": "Transfer of " + str(send_amount) + " from " + str(self.identifier) +
+                          " to " + str(destination) + " successful!"}
 
     def receive_coins(self, source, get_amount):
         # if no wallet has been created it will not be possible to receive points
         if self.get_key() == 'not activated':
             return 'This wallet was not created.'
+        # TODO: Update the amount in the database
         # if the wallet is valid the current amount plus the get_amount is the new amount
         self.amount += get_amount
-        return str(self.identifier) + ' received ' + str(get_amount) + ' from ' + str(source)
+        return {"status": str(self.get_identifier()) + " received " + str(get_amount) + " from " + str(source)}
 
 
 def handle(endpoint, data):
@@ -67,24 +85,53 @@ def handle(endpoint, data):
     :param data: nothing specified
     :return: Response of the wallet for status of requests create, get, send, receive
     """
-    # creates an unified unique identifier to identify each wallet, also not activated ones
-    unique_id = str(uuid.uuid4())
+    user_name = ""
+    wallet_key = ""
+    source = ""
+    destination = ""
+    get_amount = 0
+    send_amount = 0
+
+    data['user_name'] = user_name
+    data['wallet_key'] = wallet_key
+    data['source'] = source
+    data['destination'] = destination
+    data['get_amount'] = get_amount
+    data['send_amount'] = send_amount
     # every time the handle method is called, a new wallet object is been created with an uuid
-    wallet = Wallet(unique_id)
+    wallet = Wallet()
     # endpoint[0] will be the action what to do in an array ['get', ...]
     if endpoint[0] == 'create':
-        wallet_response = wallet.create_wallet()
+        wallet_response = wallet.create_wallet(user_name)
     elif endpoint[0] == 'get':
-        wallet_response = wallet.get_balance()
+        wallet_response = wallet.get_balance(user_name, wallet_key)
     elif endpoint[0] == 'send':
-        wallet_response = wallet.send_coins('wallet_key', 'destination', 123454321)
+        wallet_response = wallet.send_coins(user_name, wallet_key, destination, send_amount)
     elif endpoint[0] == 'receive':
-        wallet_response = wallet.receive_coins('source', 123454321)
+        wallet_response = wallet.receive_coins(source, get_amount)
     else:
         wallet_response = 'Endpoint is not supported.'
-    return {"wallet_response": wallet_response, "data": data}
+    return {"wallet_response": wallet_response, "input_data": data}
 
 
 if __name__ == '__main__':
+    """
+    # connection to database
+    connection = sqlite3.connect("wallet.db")
+
+    # create data cursor
+    cursor = connection.cursor()
+
+    # create table
+    sql = "CREATE TABLE wallet(" \
+          "id INTEGER PRIMARY KEY, " \
+          "user_name TEXT, " \
+          "wallet_key TEXT, " \
+          "uuid TEXT, " \
+          "balance REAL) "
+    
+    # execute the sql
+    cursor.execute(sql)
+    """
     m = MicroService('wallet', handle)
     m.run()
